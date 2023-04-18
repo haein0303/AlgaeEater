fb771e5bee7848aadf5e5836046331cb789abbce
ugi00 <71213609+ugi00@users.noreply.github.com>
2023-04-18 19:32:35 +0900

    npc
    임시

--- Game/Server_work/hello.lua
+++ Game/Server_work/hello.lua
@@ -15,3 +15,12 @@ end
 function create_cube( target_id )
 		API_Cube(target_id)
 end
+
+function tracking_player( player_id )
+	player_x = API_get_x(player_id)
+	player_z = API_get_z(player_id)
+	npc_x = API_get_x(player_id)
+	npc_z = API_get_z(player_id)
+
+
+end
