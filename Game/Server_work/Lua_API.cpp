3c6096e689d0cc5de88a6e2532468416d461505f
ugi00 <plis1336@naver.com>
2023-05-31 23:13:44 +0900

    server fix fix

--- Game/Server_work/Lua_API.cpp
+++ Game/Server_work/Lua_API.cpp
@@ -147,119 +147,112 @@ void close_lua(int npc_id)
 {
 	cout << "아무도 없음" << endl;
 
-	for (int i = MAX_USER; i < MAX_USER + NPC_NUM; ++i) {
-		lua_close(clients[i].L);
-		clients[i]._s_state = ST_INGAME;
-		clients[i]._Room_Num = (i - MAX_USER) / ROOM_NPC;
-		clients[i].y = 0;
-		clients[i].degree = 0;
-		clients[i].start_x = 0;
-		clients[i].start_z = 0;
-		clients[i].hp = 100;
-		clients[i].char_state = AN_IDLE;
-		clients[i]._name[0] = 0;
-		clients[i]._prev_remain = 0;
-		clients[i].Lua_on = false;
-		clients[i].stage = 0;
-		clients[i].turn = 0;
-		clients[i].room_list.clear();
-		clients[i].object_list.clear();
+	lua_close(clients[npc_id].L);
+	clients[npc_id]._s_state = ST_INGAME;
+	clients[npc_id]._Room_Num = (npc_id - MAX_USER) / ROOM_NPC;
+	clients[npc_id].y = 0;
+	clients[npc_id].degree = 0;
+	clients[npc_id].start_x = 0;
+	clients[npc_id].start_z = 0;
+	clients[npc_id].hp = 100;
+	clients[npc_id].char_state = AN_IDLE;
+	clients[npc_id]._name[0] = 0;
+	clients[npc_id]._prev_remain = 0;
+	clients[npc_id].Lua_on = false;
+	clients[npc_id].stage = 0;
+	clients[npc_id].turn = 0;
+	clients[npc_id].room_list.clear();
+	clients[npc_id].object_list.clear();
 
-		for (int j = 0; j < CUBE_NUM; j++) {
-			if (cubes[j]._Room_Num != clients[i]._Room_Num) continue;
-			else clients[i].object_list.insert(j);
-		}
+	clients[npc_id].L = luaL_newstate();
 
-		clients[i].L = luaL_newstate();
+	luaL_openlibs(clients[npc_id].L);
 
-		luaL_openlibs(clients[i].L);
+	luaL_loadfile(clients[npc_id].L, "hello.lua");
+	lua_pcall(clients[npc_id].L, 0, 0, 0);
 
-		luaL_loadfile(clients[i].L, "hello.lua");
-		lua_pcall(clients[i].L, 0, 0, 0);
+	lua_getglobal(clients[npc_id].L, "set_object_id");
+	lua_pushnumber(clients[npc_id].L, npc_id);
+	lua_pcall(clients[npc_id].L, 1, 0, 0);
 
-		lua_getglobal(clients[i].L, "set_object_id");
-		lua_pushnumber(clients[i].L, i);
-		lua_pcall(clients[i].L, 1, 0, 0);
+	lua_register(clients[npc_id].L, "API_get_x", API_get_x);
+	lua_register(clients[npc_id].L, "API_get_z", API_get_z);
+	lua_register(clients[npc_id].L, "API_get_npc_x", API_get_npc_x);
+	lua_register(clients[npc_id].L, "API_get_npc_z", API_get_npc_z);
+	lua_register(clients[npc_id].L, "API_Tracking", API_Tracking);
+	lua_register(clients[npc_id].L, "API_Return", API_Return);
 
-		lua_register(clients[i].L, "API_get_x", API_get_x);
-		lua_register(clients[i].L, "API_get_z", API_get_z);
-		lua_register(clients[i].L, "API_get_npc_x", API_get_npc_x);
-		lua_register(clients[i].L, "API_get_npc_z", API_get_npc_z);
-		lua_register(clients[i].L, "API_Tracking", API_Tracking);
-		lua_register(clients[i].L, "API_Return", API_Return);
+	int st = (npc_id - MAX_USER - clients[npc_id]._Room_Num * ROOM_NPC);
 
-		int st = (i - MAX_USER - clients[i]._Room_Num * ROOM_NPC);
+	if (st > 9) clients[npc_id]._object_type = TY_HOLD_NPC;
+	else clients[npc_id]._object_type = TY_MOVE_NPC;
 
-		if (st > 9) clients[i]._object_type = TY_HOLD_NPC;
-		else clients[i]._object_type = TY_MOVE_NPC;
+	switch (st)
+	{
+	case 10:
+	case 0:
+		clients[npc_id].x = -20;
+		clients[npc_id].z = -20;
+		break;
+	case 11:
+	case 1:
+		clients[npc_id].x = -20;
+		clients[npc_id].z = 0;
+		break;
+	case 12:
+	case 2:
+		clients[npc_id].x = -20;
+		clients[npc_id].z = 20;
+		break;
+	case 13:
+	case 3:
+		clients[npc_id].x = 0;
+		clients[npc_id].z = -20;
+		break;
+	case 14:
+	case 4:
+		clients[npc_id].x = 0;
+		clients[npc_id].z = 0;
+		break;
+	case 15:
+	case 5:
+		clients[npc_id].x = 0;
+		clients[npc_id].z = 20;
+		break;
+	case 16:
+	case 6:
+		clients[npc_id].x = 20;
+		clients[npc_id].z = -20;
+		break;
+	case 17:
+	case 7:
+		clients[npc_id].x = 20;
+		clients[npc_id].z = 0;
+		break;
+	case 18:
+	case 8:
+		clients[npc_id].x = 20;
+		clients[npc_id].z = 20;
+		break;
+	case 19: // 얘가 돌진하는 애
+	{
+		clients[npc_id].x = 30;
+		clients[npc_id].z = 30;
+		clients[npc_id].eye_color = 0;
+		clients[npc_id]._object_type = TY_BOSS_1;
 
-		switch (st)
-		{
-		case 10:
-		case 0:
-			clients[i].x = -20;
-			clients[i].z = -20;
-			break;
-		case 11:
-		case 1:
-			clients[i].x = -20;
-			clients[i].z = 0;
-			break;
-		case 12:
-		case 2:
-			clients[i].x = -20;
-			clients[i].z = 20;
-			break;
-		case 13:
-		case 3:
-			clients[i].x = 0;
-			clients[i].z = -20;
-			break;
-		case 14:
-		case 4:
-			clients[i].x = 0;
-			clients[i].z = 0;
-			break;
-		case 15:
-		case 5:
-			clients[i].x = 0;
-			clients[i].z = 20;
-			break;
-		case 16:
-		case 6:
-			clients[i].x = 20;
-			clients[i].z = -20;
-			break;
-		case 17:
-		case 7:
-			clients[i].x = 20;
-			clients[i].z = 0;
-			break;
-		case 18:
-		case 8:
-			clients[i].x = 20;
-			clients[i].z = 20;
-			break;
-		case 19: // 얘가 돌진하는 애
-		{
-			clients[i].x = 30;
-			clients[i].z = 30;
-			clients[i].eye_color = 0;
-			clients[i]._object_type = TY_BOSS_1;
-
-			lua_register(clients[i].L, "API_Wander", API_Wander);
-			lua_register(clients[i].L, "API_Rush", API_Rush);
-			lua_register(clients[i].L, "API_Cube", API_Cube);
-			lua_register(clients[i].L, "API_get_state", API_get_state);
-			break;
-		}
-		default:
-			break;
-		}
-
-		clients[i].start_x = clients[i].x;
-		clients[i].start_z = clients[i].z;
+		lua_register(clients[npc_id].L, "API_Wander", API_Wander);
+		lua_register(clients[npc_id].L, "API_Rush", API_Rush);
+		lua_register(clients[npc_id].L, "API_Cube", API_Cube);
+		lua_register(clients[npc_id].L, "API_get_state", API_get_state);
+		break;
 	}
+	default:
+		break;
+	}
+
+	clients[npc_id].start_x = clients[npc_id].x;
+	clients[npc_id].start_z = clients[npc_id].z;
 
 	for (int i = MAX_USER; i < MAX_USER + NPC_NUM; ++i) {
 		for (int j = i + 1; j < i + ROOM_NPC; ++j) {
