5b0f98429264c99d30b851fc8c40159d29678fa9
ugi00 <plis1336@naver.com>
2023-07-04 01:54:30 +0900

    lobby server fix

--- lobbyserver/lobbyserver/main.cpp
+++ lobbyserver/lobbyserver/main.cpp
@@ -10,6 +10,7 @@
 #include <chrono>
 #include <random>
 #include <windows.h>  
+#include <sqlext.h>  
 #include <locale.h>
 #include "protocol.h"
 
@@ -21,10 +22,14 @@
 #pragma comment(lib, "MSWSock.lib")
 using namespace std;
 
+#define DATA_LEN 30
+
 
 enum COMP_TYPE { OP_ACCEPT, OP_RECV, OP_SEND, OP_NPC_MOVE, OP_NPC_RUSH, OP_CREATE_CUBE, OP_UPDATE };
 
 enum SESSION_STATE { ST_FREE, ST_ACCEPTED, ST_INGAME };
+
+enum ROOM_STATE { RO_READY, RO_INGAME };
 
 class OVER_EXP {
 public:
@@ -104,8 +109,15 @@ void SESSION::send_login_ok_packet(int c_id)
 	do_send(&p);
 }
 
-array<SESSION, MAX_USER> clients;
+class USER_DATA {
+public:
+	char user_id[DATA_LEN];
+	char user_passward[DATA_LEN];
+	int user_level;
+};
 
+array<SESSION, MAX_USER> clients;
+array<USER_DATA, MAX_USER> user_datas;
 
 HANDLE g_h_iocp;
 SOCKET g_s_socket;
@@ -232,7 +244,6 @@ void process_packet(int c_id, char* packet)
 
 enum EVENT_TYPE { EV_MATCH };
 mutex timer_l;
-
 
 using namespace chrono;
 
@@ -397,8 +408,108 @@ void do_worker()
 	}
 }
 
+void show_error() {
+	printf("error\n");
+}
+
+void Data_read() {
+	SQLHENV henv;
+	SQLHDBC hdbc;
+	SQLHSTMT hstmt = 0;
+	SQLRETURN retcode;
+	SQLWCHAR szuser_id[DATA_LEN], szuser_passward[DATA_LEN];
+	SQLINTEGER duser_level;
+	SQLLEN cbID = 0, cbPASSWARD = 0, cbLEVEL = 0;
+
+	// Allocate environment handle  
+	retcode = SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &henv);
+
+	// Set the ODBC version environment attribute  
+	if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+		retcode = SQLSetEnvAttr(henv, SQL_ATTR_ODBC_VERSION, (SQLPOINTER*)SQL_OV_ODBC3, 0);
+
+		// Allocate connection handle  
+		if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+			retcode = SQLAllocHandle(SQL_HANDLE_DBC, henv, &hdbc);
+
+			// Set login timeout to 5 seconds  
+			if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+				SQLSetConnectAttr(hdbc, SQL_LOGIN_TIMEOUT, (SQLPOINTER)5, 0);
+
+				// Connect to data source  
+				retcode = SQLConnect(hdbc, (SQLWCHAR*)L"AlgaeEater_ODBC", SQL_NTS, (SQLWCHAR*)NULL, 0, NULL, 0);
+
+				// Allocate statement handle  
+				if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+					cout << "db 로딩 완료" << endl;
+					retcode = SQLAllocHandle(SQL_HANDLE_STMT, hdbc, &hstmt);
+
+					retcode = SQLExecDirect(hstmt, (SQLWCHAR*)L"SELECT user_id, user_passward, user_level FROM user_data ORDER BY 1", SQL_NTS);
+					if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+						cout << "select OK" << endl;
+						// Bind columns 1, 2, and 3  
+						retcode = SQLBindCol(hstmt, 1, SQL_C_WCHAR, szuser_id, DATA_LEN, &cbID);
+						retcode = SQLBindCol(hstmt, 2, SQL_C_WCHAR, szuser_passward, DATA_LEN, &cbPASSWARD);
+						retcode = SQLBindCol(hstmt, 3, SQL_C_LONG, &duser_level, 100, &cbLEVEL);
+						// Fetch and print each row of data. On an error, display a message and exit.  
+						for (int i = 0; ; i++) {
+							retcode = SQLFetch(hstmt);
+							if (retcode == SQL_ERROR || retcode == SQL_SUCCESS_WITH_INFO)
+								show_error();
+							if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO)
+							{
+								//replace wprintf with printf
+								//%S with %ls
+								//warning C4477: 'wprintf' : format string '%S' requires an argument of type 'char *'
+								//but variadic argument 2 has type 'SQLWCHAR *'
+								//wprintf(L"%d: %S %S %S\n", i + 1, sCustID, szName, szPhone);  
+								wprintf(L"%d: %ls %ls %d\n", i + 1, szuser_id, szuser_passward, duser_level);
+
+								wchar_t* t = reinterpret_cast<wchar_t*>(szuser_id);
+								int t_len = WideCharToMultiByte(CP_ACP, 0, t, -1, NULL, 0, NULL, NULL);
+								char* ptr;
+								ptr = new char[t_len];
+								WideCharToMultiByte(CP_ACP, 0, t, -1, ptr, t_len, 0, 0);
+								strcpy(user_datas[i].user_id, ptr);
+								delete ptr;
+
+								t = reinterpret_cast<wchar_t*>(szuser_passward);
+								t_len = WideCharToMultiByte(CP_ACP, 0, t, -1, NULL, 0, NULL, NULL);
+								ptr = new char[t_len];
+								WideCharToMultiByte(CP_ACP, 0, t, -1, ptr, t_len, 0, 0);
+								strcpy(user_datas[i].user_passward, ptr);
+								delete ptr;
+
+								user_datas[i].user_level = duser_level;
+
+								wcout << user_datas[i].user_id << "   " << user_datas[i].user_passward << "   " << user_datas[i].user_level << endl;
+							}			
+							else		
+								break;
+						}
+					}
+
+					// Process data  
+					if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+						SQLCancel(hstmt);
+						SQLFreeHandle(SQL_HANDLE_STMT, hstmt);
+					}
+
+					SQLDisconnect(hdbc);
+				}
+
+				SQLFreeHandle(SQL_HANDLE_DBC, hdbc);
+			}
+		}
+		SQLFreeHandle(SQL_HANDLE_ENV, henv);
+	}
+}
+
 int main()
 {
+	Data_read();
+
+	cout << "데이터 로딩 완료" << endl;
 	WSADATA WSAData;
 	WSAStartup(MAKEWORD(2, 2), &WSAData);
 	g_s_socket = WSASocket(AF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
--- lobbyserver/lobbyserver/protocol.h
+++ lobbyserver/lobbyserver/protocol.h
@@ -5,30 +5,59 @@ constexpr int PORT_NUM = 4000;
 constexpr int BUF_SIZE = 200;
 constexpr int NAME_SIZE = 20;
 
+enum BOSS_NUM {
+	TEST,
+	STAGE1,
+	STAGE2,
+	STAGE3,
+	COUNT
+};
+constexpr int BOSS_HP[4] = { 200000,200000,200000,200000 };
+
+
 #define LOBBY_SERVER_IP "127.0.0.1"
 #define GAME_SERVER_IP "127.0.0.1"
 constexpr int GAME_SERVER_PORT_NUM = 4000;
 constexpr int LOBBY_SERVER_PORT_NUM = 4001;
 
 constexpr int ROOM_NUM = 100;
-constexpr int MAX_USER = ROOM_NUM * 4;
-constexpr int NPC_NUM = ROOM_NUM * 10;
-constexpr int CUBE_NUM = ROOM_NUM * 4;
+constexpr int ROOM_USER = 4;
+constexpr int ROOM_NPC = 20;
+constexpr int ROOM_CUBE = 4;
+constexpr int ROOM_KEY = 4;
+constexpr int ROOM_FIELD = 10;
+
+constexpr int MAX_USER = ROOM_NUM * ROOM_USER;
+constexpr int NPC_NUM = ROOM_NUM * ROOM_NPC;
+constexpr int CUBE_NUM = ROOM_NUM * ROOM_CUBE;
+constexpr int KEY_NUM = ROOM_NUM * ROOM_KEY;
+constexpr int FIELD_NUM = ROOM_NUM * ROOM_FIELD;
 
 // Packet ID
 constexpr char CS_LOGIN = 0;
 constexpr char CS_MOVE = 1;
 constexpr char CS_CONSOLE = 2;
 constexpr char CS_COLLISION = 3;
+constexpr char CS_KEY = 4;
+constexpr char CS_OBJECT_COLLISION = 5;
+constexpr char CS_GOD_MOD = 9;
 
-constexpr char SC_LOGIN_OK = 11;
-constexpr char SC_ADD_OBJECT = 12;
-constexpr char SC_REMOVE_OBJECT = 13;
-constexpr char SC_MOVE_OBJECT = 14;
-constexpr char SC_ADD_CUBE = 15;
+constexpr char SC_LOGIN_OK = 10;
+constexpr char SC_ADD_OBJECT = 11;
+constexpr char SC_REMOVE_OBJECT = 12;
+constexpr char SC_MOVE_OBJECT = 13;
+constexpr char SC_ADD_CUBE = 14;
+constexpr char SC_ADD_BOSS = 15;
+constexpr char SC_MOVE_BOSS = 16;
+constexpr char SC_MSG = 17;
+constexpr char SC_KEY = 18;
+constexpr char SC_DOOR = 19;
+///////////////////////////////
+constexpr char SC_BOSS_SKILL_START = 20;
+constexpr char SC_BOSS_SKILL_END = 21;
 
-constexpr char SS_CONNECT_SERVER = 20;
-constexpr char SS_DATA_PASS = 21;
+constexpr char SS_CONNECT_SERVER = 28;
+constexpr char SS_DATA_PASS = 29;
 
 constexpr char LCS_LOGIN = 30;
 constexpr char LCS_MATCH = 31;
@@ -67,9 +96,29 @@ struct CS_CONSOLE_PACKET {
 struct CS_COLLISION_PACKET {
 	unsigned char size;
 	char	type;
-	char	attack_type;
+	int		attack_type;
 	int		attacker_id;
 	int		target_id;
+};
+
+struct CS_KEY_PACKET {
+	unsigned char size;
+	char	type;
+	int		color;
+	int		key_id;
+};
+
+struct CS_OBJECT_COLLISION_PACKET {
+	unsigned char size;
+	char	type;
+	int		attacker_id;
+	int		target_id;
+	int		object_type; // 0은 큐브 1은 장판
+};
+
+struct CS_GOD_MOD_PACKET {
+	unsigned char size;
+	char type;
 };
 
 struct SC_LOGIN_OK_PACKET {
@@ -90,16 +139,7 @@ struct SC_ADD_OBJECT_PACKET {
 	char	name[NAME_SIZE];
 	int		hp;
 	int		char_state;
-};
-
-struct SC_ADD_CUBE_PACKET {
-	unsigned char size;
-	char	type;
-	int		id;
-	float	x, y, z;
-	float	degree;
-	char	name[NAME_SIZE];
-	int		hp;
+	int		object_type;
 };
 
 struct SC_REMOVE_OBJECT_PACKET {
@@ -116,6 +156,78 @@ struct SC_MOVE_OBJECT_PACKET {
 	float	degree;
 	int		hp;
 	int		char_state;
+	unsigned int client_time;
+};
+
+struct SC_ADD_CUBE_PACKET {
+	unsigned char size;
+	char	type;
+	int		id;
+	float	x, y, z;
+	float	degree;
+	char	name[NAME_SIZE];
+	int		color;
+	int		hp;
+};
+
+struct SC_ADD_BOSS_PACKET {
+	unsigned char size;
+	char	type;
+	int		id;
+	float	x, y, z;
+	float	degree;
+	char	name[NAME_SIZE];
+	int		hp;
+	int		char_state;
+};
+
+struct SC_MOVE_BOSS_PACKET {
+	unsigned char size;
+	char	type;
+	int		id;
+	float	x, y, z;
+	float	degree;
+	int		hp;
+	int		char_state;
+	int		eye_color;
+	unsigned int client_time;
+};
+
+struct SC_MSG_PACKET {
+	unsigned char size;
+	char	type;
+	char	msg[NAME_SIZE];
+};
+
+struct SC_KEY_PACKET {
+	unsigned char size;
+	char	type;
+	float	x;
+	float	y;
+	float	z;
+	int		id;
+	int		color;
+};
+
+struct SC_DOOR_PACKET {
+	unsigned char size;
+	char	type;
+};
+
+struct SC_BOSS_SKILL_START_PACKET {
+	unsigned char size;
+	char	type;
+	int		id;
+	float	x;
+	float	z;
+	float	r;
+	int		fd_type;
+	int		char_state;
+};
+
+struct SC_BOSS_SKILL_END_PACKET {
+	unsigned char size;
+	char	type;
 };
 
 struct SS_CONNECT_SERVER_PACKET {
