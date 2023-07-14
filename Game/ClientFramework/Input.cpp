41640f9a63eab103ed299a5ddaac88a561b9bff5
CPUAndGPU <cpugpu131@gmail.com>
2023-07-14 17:46:43 +0900

    Stage2 Map Rendering

--- Game/ClientFramework/Input.cpp
+++ Game/ClientFramework/Input.cpp
@@ -215,14 +215,14 @@ void Input::InputKey(shared_ptr<Timer> timerPtr, array<OBJECT, PLAYERMAX>& playe
 
 				for (const BoundingBox& bounding_box : bounding_boxes)
 				{
-					if (playerArr[networkPtr->myClientId]._bounding_box.Intersects(bounding_box))
+					/*if (playerArr[networkPtr->myClientId]._bounding_box.Intersects(bounding_box))
 					{
 						playerArr[networkPtr->myClientId]._transform.x = pos_x0;
 						playerArr[networkPtr->myClientId]._transform.z = pos_z0;
 
 						playerArr[networkPtr->myClientId]._bounding_box.Center.x = pos_x0;
 						playerArr[networkPtr->myClientId]._bounding_box.Center.z = pos_z0;
-					}
+					}*/
 				}
 
 				for (int i = 0; i < playerArr.size(); ++i)
